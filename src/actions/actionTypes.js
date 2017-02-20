ff124fccfbfb95740dc84ef14ab7cdf4fb1b2074
Camilla Tabis <Camilla Tabis>
2017-02-20 13:30:35 -0500

    Get title and city printing

--- src/actions/actionTypes.js
+++ src/actions/actionTypes.js
@@ -1,8 +1,12 @@
 export const ACTION_TYPES = {
   LOAD_EVENTS_SUCCESS: 'LOAD_EVENTS_SUCCESS',
 <<<<<<< HEAD
+<<<<<<< HEAD
   LOAD_EVENT_DETAILS: 'LOAD_EVENT_DETAILS',
 =======
   LOAD_EVENTS_DETAILS: 'LOAD_EVENT_DETAILS',
 >>>>>>> adding event details that is able to get details object in route /EventDetails
+=======
+  LOAD_EVENT_DETAILS: 'LOAD_EVENT_DETAILS',
+>>>>>>> Get title and city printing
 };
