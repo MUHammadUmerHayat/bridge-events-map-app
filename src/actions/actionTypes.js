532132a1718fdcd4d3752a855b6d4c42fc1cecdd
Camilla Tabis <Camilla Tabis>
2017-02-20 13:10:16 -0500

    adding event details that is able to get details object in route /EventDetails

--- src/actions/actionTypes.js
+++ src/actions/actionTypes.js
@@ -1,4 +1,8 @@
 export const ACTION_TYPES = {
   LOAD_EVENTS_SUCCESS: 'LOAD_EVENTS_SUCCESS',
+<<<<<<< HEAD
   LOAD_EVENT_DETAILS: 'LOAD_EVENT_DETAILS',
+=======
+  LOAD_EVENTS_DETAILS: 'LOAD_EVENT_DETAILS',
+>>>>>>> adding event details that is able to get details object in route /EventDetails
 };
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,7 +2,11 @@ import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 import events from './eventReducer';
 import { reducer as formReducer } from 'redux-form';
+<<<<<<< HEAD
 import LoadDetailsReducer from './eventDetailsReducer';
+=======
+import LoadDetailsReducer from './event-details';
+>>>>>>> adding event details that is able to get details object in route /EventDetails
 
 const rootReducer = combineReducers({
   routing: routerReducer,
