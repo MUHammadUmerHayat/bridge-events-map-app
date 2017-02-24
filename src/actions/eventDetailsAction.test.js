3ace1651b00ee04909252d9f4b413b8dab222999
Camilla Tabis <Camilla Tabis>
2017-02-23 19:30:47 -0500

    fix linting errors

--- src/actions/eventDetailsAction.test.js
+++ src/actions/eventDetailsAction.test.js
@@ -2,7 +2,7 @@ import * as actions from './eventDetailsActions';
 import * as types from './actionTypes';
 
 describe('actions', () => {
-  it('should create an action to add a todo', () => {
+  it('should create an action to load event details', () => {
     const response = { title: 'Mariah Carey', city: 'Toronto'};
     const expectedAction = {
       type: types.ACTION_TYPES.LOAD_EVENT_DETAILS,
