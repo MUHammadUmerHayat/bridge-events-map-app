77d8eb823494bc82529d513dffee582049233af3
Camilla Tabis <Camilla Tabis>
2017-02-23 19:27:37 -0500

    fix linting errors

--- src/actions/eventDetailsAction.test.js
+++ src/actions/eventDetailsAction.test.js
@@ -1,13 +1,13 @@
-import * as actions from './eventDetailsActions'
-import * as types from './actionTypes'
+import * as actions from './eventDetailsActions';
+import * as types from './actionTypes';
 
 describe('actions', () => {
   it('should create an action to add a todo', () => {
-    const response = { title: 'Mariah Carey', city: 'Toronto'}
+    const response = { title: 'Mariah Carey', city: 'Toronto'};
     const expectedAction = {
       type: types.ACTION_TYPES.LOAD_EVENT_DETAILS,
-      payload : { details : {title: 'Mariah Carey', city: 'Toronto'}}
-    }
-    expect(actions.getEvent(response)).toEqual(expectedAction)
-  })
-})
+      payload: { details: {title: 'Mariah Carey', city: 'Toronto'}};
+    };
+    expect(actions.getEvent(response)).toEqual(expectedAction);
+  });
+});
