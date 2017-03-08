05c3791089edeb0ee9cd741619f20293f8063457
Camilla <camilla.tabis@gmail.com>
2017-03-07 20:56:20 -0500

    Add map to Event Details page (#27)
    * Add map to Event Details page
    
    * add image to event details page
    
    *  make image a thumbnail

--- src/reducers/eventDetailsReducer.js
+++ src/reducers/eventDetailsReducer.js
@@ -3,8 +3,16 @@ import * as types from '../actions/actionTypes';
 export default function(state = {}, action) {
   switch (action.type) {
 
-  case types.ACTION_TYPES.LOAD_EVENT_DETAILS:
-    return action.payload.details;
+  case types.ACTION_TYPES.LOAD_EVENT_DETAILS: {
+    let details = action.payload.details;
+    if (details.images) {
+      if (details.images.image) {
+        details = Object.assign({}, action.payload.details, { imageUrl: action.payload.details.images.image.url});
+      }
+    }
+    return details;
+  }
+
 
   default:
     return state;
